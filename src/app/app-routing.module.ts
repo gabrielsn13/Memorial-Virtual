4ccda37dbbbb97fa132f76620485ee964c03aa62
gabriel.neves <gabriel.neves@comlink.com.br>
2025-12-02 12:50:13 -0300

    add componente signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+  },
+  {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
---
+++ src/app/signup/signup.page.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: 'signup.page.html',
+  styleUrls: ['signup.page.scss'],
+  standalone: false,
+})
+export class SignupPage {
+  birthday = {
+    month: '',
+    day: '',
+    year: ''
+  };
+
+  phone = {
+    countryCode: 'BR +55',
+    number: ''
+  };
+
+  verificationCode = '';
+  showCodeInput = false;
+
+  months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  days: number[] = Array.from({ length: 31 }, (_, i) => i + 1);
+  years: number[] = Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i);
+
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
+
+  sendCode() {
+    if (this.phone.number) {
+      this.showCodeInput = true;
+      // Aqui você implementaria a lógica de envio do código
+      console.log('Code sent to:', this.phone.countryCode, this.phone.number);
+    }
+  }
+
+  signUpWithEmail() {
+    // Implementar navegação para cadastro com email
+    console.log('Sign up with email');
+  }
+
+  next() {
+    if (this.showCodeInput && this.verificationCode) {
+      // Validar código e prosseguir
+      console.log('Verification code:', this.verificationCode);
+      // Aqui você navegaria para a próxima etapa ou finalizaria o cadastro
+    } else if (this.birthday.month && this.birthday.day && this.birthday.year && this.phone.number) {
+      this.sendCode();
+    }
+  }
+
+  isFormValid(): boolean {
+    if (this.showCodeInput) {
+      return this.verificationCode.length === 6;
+    }
+    return !!(this.birthday.month && this.birthday.day && this.birthday.year && this.phone.number);
+  }
+}
+
