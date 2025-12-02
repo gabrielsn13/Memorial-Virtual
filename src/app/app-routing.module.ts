742e2a57106757490e82a3fde97da89650e8ee92
gabriel.neves <gabriel.neves@comlink.com.br>
2025-12-02 13:00:54 -0300

    add tela inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
