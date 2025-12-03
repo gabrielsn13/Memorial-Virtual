fe4d7246140edf06ebab41feb501eab0fcaeb911
gabriel.neves <gabriel.neves@comlink.com.br>
2025-12-03 10:18:22 -0300

    add arquivos para configuracoes do projeto

---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.afteer.app',
+  appName: 'Afteer',
+  webDir: 'www'
+};
+
+export default config;
